5b74266614ba007a210b52c343844b7af361884f
blackbracken <me@bracken.black>
2022-09-13 00:38:46 +0900

    set theme

--- app/src/main/java/black/bracken/zeropoint/MainActivity.kt
+++ app/src/main/java/black/bracken/zeropoint/MainActivity.kt
@@ -3,26 +3,41 @@ package black.bracken.zeropoint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import black.bracken.zeropoint.ui.theme.ZeroPointTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     setContent {
       ZeroPointTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background
         ) {
-          Greeting("Android")
+          Column {
+            repeat(4) {
+              Button(onClick = {}) {
+                Text("Button 1")
+              }
+
+              Spacer(Modifier.height(8.dp))
+            }
+
+            Greeting("Android")
+          }
         }
       }
     }
