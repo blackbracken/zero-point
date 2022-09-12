8cdab3f5073a27f2e4557c7fdd7beda4198d6bc8
blackbracken <me@bracken.black>
2022-09-12 22:55:31 +0900

    manage library with version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,16 +57,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("androidx.compose.ui:ui:1.2.0-beta01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0-beta01")
-    implementation("androidx.compose.material:material:1.2.0-beta01")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0-beta01")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.0-beta01")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.0-beta01")
+    implementation(libs.androidxCoreKtx)
+    implementation(libs.androidxLifecycleRuntimeKtx)
+    implementation(libs.androidxActivityCompose)
+    implementation(libs.androidxComposeUi)
+    implementation(libs.androidxComposeUiToolingPreview)
+    implementation(libs.androidxComposeMaterial)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidxTestExtJunit)
+    androidTestImplementation(libs.androidxTestEspressoCore)
+    androidTestImplementation(libs.androidxComposeUiTestJunit4)
+    debugImplementation(libs.androidxComposeUiTooling)
+    debugImplementation(libs.androidxComposeUiTestManifest)
 }
