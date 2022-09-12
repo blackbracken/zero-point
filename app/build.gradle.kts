d94d250b5ebf2872e017711d80056814a1caf4a5
blackbracken <me@bracken.black>
2022-09-12 22:23:54 +0900

    format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,19 +33,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.2.0-beta01"
     }
+
     packagingOptions {
         jniLibs.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
     }
