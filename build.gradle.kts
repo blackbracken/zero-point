ff21147ffd91b62889fb31d160e60561bd4ae018
blackbracken <me@bracken.black>
2022-09-12 21:26:02 +0900

    integrate kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+//    id("com.android.application") version "7.4.0-alpha09"
+//    id("com.android.library") version "7.4.0-alpha09"
+//    id("org.jetbrains.kotlin.android") version "1.6.21"
+}
