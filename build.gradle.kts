3e75dcf75958352b9263e1a099b27354381a1add
blackbracken <me@bracken.black>
2022-09-21 23:45:15 +0900

    chore: rename ktlint-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,12 @@ plugins {
   alias(libs.plugins.androidApplication) apply false
   alias(libs.plugins.androidLibrary) apply false
   alias(libs.plugins.kotlinAndroid) apply false
-
-  id(libs.plugins.ktlint.get().pluginId) apply false
+  alias(libs.plugins.ktlintGradle) apply false
 }
 
 allprojects {
   afterEvaluate { // for version catalog
-    apply(plugin = libs.plugins.ktlint.get().pluginId)
+    apply(plugin = libs.plugins.ktlintGradle.get().pluginId)
 
     configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
       debug.set(true)
