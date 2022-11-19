25c76cefa4fce3f7e539b1e0cd1f926d72f17c37
blackbracken <me@bracken.black>
2022-11-19 16:26:44 +0900

    chore(ChooseSource): prepare color tokens

---
+++ app/src/main/java/black/bracken/zeropoint/ui/theme/ZeroColorTokens.kt
@@ -0,0 +1,10 @@
+package black.bracken.zeropoint.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ZeroColorTokens {
+  val valorantRed = Color(0xFFEB2C45)
+
+  val objectMediumEmphasis = Color.Gray
+  val objectHighEmphasis = Color.DarkGray
+}
