eef408713de6bbe59f584af1dd479b17eb992679
blackbracken <me@bracken.black>
2022-11-26 21:17:33 +0900

    fix: set ime padding for modal

--- feature/setup/src/main/kotlin/black/bracken/zeropoint/feature/setup/choosesource/ChooseSourceScreen.kt
+++ feature/setup/src/main/kotlin/black/bracken/zeropoint/feature/setup/choosesource/ChooseSourceScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -277,7 +278,8 @@ private fun InputPlayerNameModalBottomSheetContent(
   Column(
     modifier = Modifier
       .fillMaxWidth()
-      .padding(horizontal = 16.dp),
+      .padding(horizontal = 16.dp)
+      .imePadding(),
   ) {
     Spacer(modifier = Modifier.height(16.dp))
 
